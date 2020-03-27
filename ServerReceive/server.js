9de5834852fe96005f1a292a9f827d2240e5ff98
ongtrumduy <phamhuyduy99@gmail.com>
2020-03-27 16:53:37 +0700

    upload

--- ServerReceive/server.js
+++ ServerReceive/server.js
@@ -1,15 +1,11 @@
-const express = require("express");
-const app = express();
 const dgram = require('dgram');
-const socketio = require("socket.io");
 const ip = require("ip");
+const io_client = require("socket.io-client");
 
-const new_server = require("http").Server(app);
-const io = socketio.listen(new_server);
+
 const server = dgram.createSocket('udp4');
 
 const port = 18181;
-new_server.listen(port);
 
 var ip_local = ip.address();
 var ip_last_index = ip_local.lastIndexOf(".");
@@ -33,13 +29,18 @@ var serial_number = "124584";
 
 server.on('message', (msg, rinfo) => {
   console.log(`Nhận: ${msg} từ ${rinfo.address}:${rinfo.port}`);
-  console.log(msg.toString('utf8'))
   var msg_convert = msg.toString("utf8");
+  console.log(msg_convert);
   if (msg_convert === "IP???") {
     console.log("Đã nhận được");
     const ip_receive_convert = rinfo.address.toString("utf8")
     ip_receive.push(ip_receive_convert);
-    server.send(Buffer.from(ip_local), rinfo.port, ip_broadcast);
+
+    var server_receive_address = "http://" + ip_receive_convert + ":" + rinfo.port;
+    console.log(server_receive_address);
+    const socket = io_client.connect(server_receive_address);
+    socket.emit("sendIP", ip_local);
+    socket.emit("sendserialnumber", serial_number);
 
     console.log("=============================================");
     console.log("Các IP đã nhận: ");
@@ -47,16 +48,10 @@ server.on('message', (msg, rinfo) => {
       console.log(item);
     })
     console.log("=============================================");
+
   }
-  io.on("connection", function (socket) {
-    io.emit("sendserialnumber", serial_number);
-    socket.on("test", function (data) {
-      console.log("Gửi trả:");
-      console.log(data); 
-    })
-  });
- 
 });
+
 
 
 
--- ServerSend/server.js
+++ ServerSend/server.js
@@ -1,24 +1,27 @@
+const express = require("express");
+const app = express();
 const dgram = require('dgram');
 const ip = require("ip");
-const io_client = require("socket.io-client");
+const socketio = require("socket.io");
 
 const port = 8000;
 
-
+const new_server = require("http").Server(app);
+const io = socketio.listen(new_server);
 const server = dgram.createSocket('udp4');
+
+new_server.listen(port);
 
 var ip_local = ip.address();
 var ip_last_index = ip_local.lastIndexOf(".");
 var ip_local_convert = ip_local.substring(0, ip_last_index + 1);
 var ip_broadcast = ip_local_convert + "255";
-var ip_receive_convert = 0;
+
 
 server.on('error', (err) => {
     console.log(`Server error:\n${err.stack}`);
     server.close();
 });
-
-
 
 server.on("listening", () => {
     console.log(`Server đang nghe ${ip_local}:${port}`);
@@ -26,35 +29,29 @@ server.on("listening", () => {
 
 server.bind(port);
 
-
 var message_send = "IP???"
-var data_receive = 0;
+var data_ip_receive = 0;
+var data_number_receive = 0;
+
 
 server.send(Buffer.from(message_send), 18181, ip_broadcast, (err) => {
     console.log(err);
 });
 
-
-server.on("message", (msg, rinfo) => {
-    console.log("Gửi yêu cầu: IP???");
-    console.log(`Nhận: ${msg} từ ${rinfo.address}:${rinfo.port}`);
-    ip_receive_convert = rinfo.address.toString("utf8");
-    console.log("IP đã nhận: ");
-    console.log(ip_receive_convert);
-
-    var server_receive_address = "http://" + ip_receive_convert + ":18181";
-
-    const socket = io_client.connect(server_receive_address);
-
-    console.log(server_receive_address);
-
-    socket.on("sendserialnumber", function (data) {
-        if (data_receive !== data) {
-            console.log("Serial number:");
+io.on("connect", function (socket) {
+    socket.on("sendIP", function (data) {
+        if (data_ip_receive !== data) {
+            console.log("Ip Server Receive:");
             console.log(data);
             socket.emit("test", "Đã nhận được rồi!!!");
-            data_receive = data;
+            data_ip_receive = data;
         }
     })
-}
-)
+    socket.on("sendserialnumber", function (data) {
+        if (data_number_receive !== data) {
+            console.log("Serial number:");
+            console.log(data);
+            data_number_receive = data;
+        }
+    })
+})
